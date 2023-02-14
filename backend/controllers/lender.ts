af55ff729d4b31676124a1588850760b9994ad79
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2023-02-14 12:53:39 +0100

    Update lender.ts

--- backend/controllers/lender.ts
+++ backend/controllers/lender.ts
@@ -61,7 +61,7 @@ export default class LenderController {
         lastname: Joi.string().required(),
         email: Joi.string().email().required(),
         phone: Joi.string().required(),
-        password: Joi.string().alphanum().min(6).required(),
+        password: Joi.string().min(6).required(),
       })
         .options({ abortEarly: false })
         .validate(input);
